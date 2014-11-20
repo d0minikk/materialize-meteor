e22a823143068e42c243077c4fd0f7f7e9e49c60
Dominik Curyło <curylo.dominik@gmail.com>
2014-11-20 20:28:44 +0100

    proper conf

--- package.js
+++ package.js
@@ -1,17 +1,31 @@
 Package.describe({
   name: 'd0minikk:materialize-meteor',
   summary: 'A modern responsive front-end framework based on Material Design',
-  version: '0.0.1',
+  version: '1.0.0',
   git: 'https://github.com/d0minikk/materialize-meteor.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('materialize-meteor.js');
-});
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('materialize-meteor');
-  api.addFiles('materialize-meteor-tests.js');
+  api.use('jquery');
+
+  var path = Npm.require('path');
+  var asset_path = path.join('materialize');
+  api.addFiles(path.join(asset_path, 'css', 'materialize.css'), 'client');
+  api.addFiles(path.join(asset_path, 'js', 'materialize.js'), 'client');
+
+  // fonts
+  api.addFiles(path.join(asset_path, 'fonts/roboto', 'Roboto-Bold.ttf'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/roboto', 'Roboto-Light.ttf'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/roboto', 'Roboto-Medium.ttf'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/roboto', 'Roboto-Regular.ttf'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/roboto', 'Roboto-Thin.ttf'), 'client');
+
+  api.addFiles(path.join(asset_path, 'fonts/material-design-icons', 'Material-Design-Icons.eot'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/material-design-icons', 'Material-Design-Icons.svg'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/material-design-icons', 'Material-Design-Icons.ttf'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts/material-design-icons', 'Material-Design-Icons.woff'), 'client');
+
 });
