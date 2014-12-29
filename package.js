69d9070001f017bd5a0abe5d25207ff643843f6a
noxan <noxan@byteweaver.org>
2014-12-29 13:25:18 +0100

    bump version number in package file to 1.0.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'd0minikk:materialize-meteor',
   summary: 'A modern responsive front-end framework based on Material Design',
-  version: '1.0.3',
+  version: '1.0.4',
   git: 'https://github.com/d0minikk/materialize-meteor.git'
 });
 
