fbeb140d7cd74f6e59836f80cbfdd2b95dedc8be
Rob Race <rob.race@traxtech.com>
2015-01-21 10:08:40 -0700

    Version bump to 0.95.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'd0minikk:materialize-meteor',
   summary: 'A modern responsive front-end framework based on Material Design',
-  version: '1.94.2',
+  version: '1.95.0',
   git: 'https://github.com/d0minikk/materialize-meteor.git'
 });
 
